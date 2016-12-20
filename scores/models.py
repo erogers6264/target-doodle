ddd8fec3a714a59a6c9ea15c3a8b6306559e31bc
Ethan Rogers <erogers6264@gmail.com>
2016-12-20 00:08:50 -0700

    Create a model for colleges

--- scores/models.py
+++ scores/models.py
@@ -3,3 +3,8 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+class College(models.Model):
+    college_name = models.CharField(max_length=50)
+    region = models.CharField(max_length=50)
+    SAT25pScore = models.IntegerField(default=0)
+    SAT75pScore = models.IntegerField(default=0)
