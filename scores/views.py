b1fa36bcd0312ad48b6e56b550a7905451ab1f78
Ethan Rogers <erogers6264@gmail.com>
2016-12-20 01:49:24 -0700

    Add missing space for clean code

--- scores/views.py
+++ scores/views.py
@@ -4,7 +4,7 @@ from .models import College
 
 def index(request):
     top_college_list = College.objects.order_by('-SAT75pScore')[:5]
-    output = ', '.join([c.college_name + ': ' +str(c.SAT75pScore) for c in top_college_list])
+    output = ', '.join([c.college_name + ': ' + str(c.SAT75pScore) for c in top_college_list])
     return HttpResponse(output)
 
 def allColleges(request):
