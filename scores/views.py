90c13b7876eb8c02f93424ba1c42cd210d42faea
Ethan Rogers <erogers6264@gmail.com>
2016-12-20 01:13:17 -0700

    Add some additional views and map to urls

--- scores/views.py
+++ scores/views.py
@@ -3,3 +3,9 @@ from django.http import HttpResponse
 
 def index(request):
   return HttpResponse("Hello, world. You're at the scores index.")
+
+def allColleges(request):
+    return HttpResponse("This page will list all colleges.")
+
+def collegeDetail(request, college_id):
+    return HttpResponse("This page the detail of college %s." % college_id)
