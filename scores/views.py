a990b4cae097e2d92f1c7a91354bcd923db38120
Ethan Rogers <erogers6264@gmail.com>
2016-12-20 01:55:02 -0700

    Modify view to utilize template

--- scores/views.py
+++ scores/views.py
@@ -1,11 +1,15 @@
 from django.http import HttpResponse
+from django.template import loader
 
 from .models import College
 
 def index(request):
     top_college_list = College.objects.order_by('-SAT75pScore')[:5]
-    output = ', '.join([c.college_name + ': ' + str(c.SAT75pScore) for c in top_college_list])
-    return HttpResponse(output)
+    template = loader.get_template('scores/index.html')
+    context = {
+        'top_college_list': top_college_list,
+    }
+    return HttpResponse(template.render(context, request))
 
 def allColleges(request):
     return HttpResponse("This page will list all colleges.")
